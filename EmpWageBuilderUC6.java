07189141ecfe41325f469238f5bc43f17d746f06
Sangita <pal07sangita@gmail.com>
2020-07-19 00:16:51 +0530

    Add UC6 - EmpWageBuilderUC6.java

---
+++ EmpWageBuilderUC6.java
@@ -0,0 +1,34 @@
+public class EmpWageBuilderUC6{
+public static final int IS_PART_TIME = 1;
+public static final int IS_FULL_TIME = 2;
+public static final int EMP_RATE_PER_HOUR = 20;
+public static final int NUM_OF_WORKING_DAYS = 20;
+public static final int MAX_HRS_IN_MONTH = 120;
+public static void main(String[] agrs){
+int emphrs = 0;
+int totalemphrs = 0;
+int empwage = 0;
+int Totalempwage = 0;
+int totalworkingdays = 0;
+while(totalemphrs <= MAX_HRS_IN_MONTH && totalworkingdays < NUM_OF_WORKING_DAYS){
+		totalworkingdays++;
+double  empcheck = Math.floor(Math.random() % 10) % 3;
+
+switch ((int)empcheck){
+  case IS_PART_TIME:
+	emphrs = 8;
+	break;
+	case IS_FULL_TIME:
+        emphrs = 12;
+	break;
+	default:
+	emphrs = 0;
+       }
+totalemphrs += emphrs;
+System.out.println("Day#: " + totalworkingdays + "Emp Hr: " +emphrs);
+}
+Totalempwage = totalemphrs * EMP_RATE_PER_HOUR;
+
+System.out.println("Total Emp Wage: " + Totalempwage);
+}
+}
