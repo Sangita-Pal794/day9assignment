46e42457b85aff95b04664554078eda2c68dbf79
Sangita <pal07sangita@gmail.com>
2020-07-18 23:51:37 +0530

    Add UC5 - EmpWageBuilderUC5.java

---
+++ EmpWageBuilderUC5.java
@@ -0,0 +1,30 @@
+public class EmpWageBuilderUC5{
+public static final int IS_PART_TIME = 1;
+public static final int IS_FULL_TIME = 2;
+public static final int EMP_RATE_PER_HOUR = 20;
+public static final int NUM_OF_WORKING_DAYS = 20;
+public static void main(String[] agrs){
+int emphrs = 0;
+int empwage = 0;
+int Totalempwage = 0;
+for(int day=0;day<=NUM_OF_WORKING_DAYS;day++){
+double  empcheck = Math.floor(Math.random() % 10) % 3;
+
+switch ((int)empcheck){
+  case IS_PART_TIME:
+	emphrs = 8;
+	break;
+	case IS_FULL_TIME:
+        emphrs = 12;
+	break;
+	default:
+	emphrs = 0;
+       }
+
+empwage = emphrs * EMP_RATE_PER_HOUR;
+Totalempwage += empwage;
+System.out.println("Emp Wage: " + empwage);
+}
+System.out.println("Total Emp Wage: " + Totalempwage);
+}
+}
