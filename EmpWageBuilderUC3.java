a2611f7f33a62f4ca6d07a95a04a4cf94b6ff43b
Sangita <pal07sangita@gmail.com>
2020-07-19 21:26:43 +0530

    Added EmpWageBuilderUC3

--- EmpWageBuilderUC3.java
+++ EmpWageBuilderUC3.java
@@ -5,7 +5,7 @@ public static void main(String[] args){
 	int EMP_RATE_PER_HOUR = 20;
 	int emphrs = 0;
 	int empwage = 0; 
-double empcheck = Math.floor(Math.random() % 10) % 3;
+double empcheck = Math.floor(Math.random() * 10) % 3;
 
 if(empcheck == IS_FULL_TIME)
 	emphrs = 8;
